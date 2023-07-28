6719039a4295f527d27ee678fd6c97d8cb1653e2
Jackwaang <122063529+Jackwaang@users.noreply.github.com>
2023-07-28 16:13:36 -0400

    Add files via upload

--- StoreManualLabels.py
+++ StoreManualLabels.py
@@ -4,15 +4,19 @@ import json
 
 postTypes = ['issues', 'issues_comment', 'pull', 'pull_comment', 'pull_review_comment', 'commit_comment']
 
-
+entriesList = []
 
 for postType in postTypes:
     input_file = postType + '.txt'
-    with open(input_file) as file, open('stored_labels.txt', 'w', encoding = 'utf-8', errors='ignore') as output:
+    with open(input_file) as file:
         for line in file:
             data = json.loads(line)
             entry = {
-                'post_id': [data['post_id']],
-                'labels': [data['labels']]
+                'post_id': data['post_id'],
+                'labels': data['labels']
             }
-            output.write(json.dumps(entry))
+            entriesList.append(json.dumps(entry))
+
+with open('stored_labels.txt', 'w', encoding = 'utf-8', errors='ignore') as output:        
+    for entry in entriesList:
+        output.write(entry + '\n')
---
+++ applyManualLabels.py
@@ -0,0 +1,44 @@
+
+import json
+
+
+postTypes = ['issues', 'issues_comment', 'pull', 'pull_comment', 'pull_review_comment', 'commit_comment']
+
+#load stored labels into entries dict
+entriesDict = dict()
+with open('stored_labels.txt') as file:
+    for line in file:
+            data = json.loads(line)
+            entriesDict[data['post_id']] = data['labels']
+
+#go through files and modify labels with stored labels. store not in entriesDict as empty
+
+#To do this, go through openaigenerated. For each line. look if id is in entries dict. 
+#If it is, have entries label be set to the labels in the dict
+#If not, have entry be unrelated.
+#Then, add entry to list of entries
+#after going through every line in file, write list of entries json dump to the non_openaigenerated file.
+
+for type in postTypes:
+    entriesList = []
+    file1name = type + '.txt'
+    file2name = type + '_openaiGenerated.txt'
+    with  open(file2name) as file2:
+        for line in file2:
+            data = json.loads(line)
+            if(data['post_id'] in entriesDict):
+                 data['labels'] = entriesDict[data['post_id']]
+            else:
+                 data['labels'] = ['TO_RESOLVE']
+            entriesList.append(json.dumps(data))
+
+    with open(file1name, 'w', encoding = 'utf-8', errors='ignore') as output:
+         for entry in entriesList:
+              output.write(entry + '\n')
+
+
+        
+            
+with open('stored_labels.txt', 'w', encoding = 'utf-8', errors='ignore') as output:        
+    for entry in entriesList:
+        output.write(entry + '\n')
