f4ca72c32c49672e09fd44095dd2b1eca44454f5
Jackwaang <122063529+Jackwaang@users.noreply.github.com>
2023-08-08 18:26:42 -0400

    Bug Fixes + results.txt generation

--- accuracy.py
+++ accuracy.py
@@ -27,46 +27,49 @@ numUnrelated = 0
 #loop through nonopenai posts
 for i in range(0, (len(list1))):
     #loop through manual labels
-    for j in range(0, len(list1[i])):
+    for j in range(0, len(list1[i][0])):
         #loop through labels in openaiGenerated file
-        for k in range(0, len(list2[i])):
-            if list1[i][j] == list2[i][k]:
+        for k in range(0, len(list2[i][0])):
+            manualLabel = list1[i][j]
+            AILabel = list2[i][k]
+            if manualLabel == AILabel:
                 numEqual += 1
             else:
-                """
+                
                 print(textList1[i][0] + " in file " + textList1[i][1])
                 print("Manual label(s) ")
                 print(list1[i][0])
                 print("AI Labels: ")
                 print(list2[i][0])
                 print("\n" )
-                """
-            if list1[i][j] == ['Unrelated']:
-                numUnrelated += 1
+                
+        if list1[i][j] == ['Unrelated']:
+            numUnrelated += 1
 
 #Loop through each post type and each label to figure out label distribution
 #loop through postTypes
-for type in postTypes:
-    print("For file type: " + type + "\n")
-    numOfType = 0
-    #loop through each Label
-    for label in labels:
-        numOfLabel = 0
-        #loop through the manual labels
-        for i in range(0, len(list1)):
-            if(list1[i][1] == type):
-                #make sure it only increments on the first iteration
-                if(label == labels[0]):
-                    numOfType += 1
-                for manualLabel in list1[i][0]:
-                    if manualLabel == label:
-                        numOfLabel += 1
-        print(str(numOfLabel) + " posts are labeled as: " + label + '\n')
+with open('results.txt', 'w', encoding = 'utf-8', errors='ignore') as output:
+    for type in postTypes:
+        output.write("For file type: " + type + "\n")
+        numOfType = 0
+        #loop through each Label
+        for label in labels:
+            numOfLabel = 0
+            #loop through the manual labels
+            for i in range(0, len(list1)):
+                if(list1[i][1] == type):
+                    #make sure it only increments on the first iteration
+                    if(label == labels[0]):
+                        numOfType += 1
+                    for manualLabel in list1[i][0]:
+                        if manualLabel == label:
+                            numOfLabel += 1
+            output.write(str(numOfLabel) + " posts are labeled as: " + label + '\n')
 
-    print("of " + str(numOfType) + " posts of type " + type + "\n" )
+        output.write("of " + str(numOfType) + " posts of type " + type + "\n" )
 
         
 
-print(str(numEqual) + '/' + str(len(list1)) + ' are matching')
-print(str(numUnrelated) + '/' + str(len(list1)) + ' are Unrelated')
+    output.write(str(numEqual) + '/' + str(len(list1)) + ' are matching, ')
+    output.write(str(numUnrelated) + '/' + str(len(list1)) + ' are Unrelated')
 
